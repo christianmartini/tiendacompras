baee11ca16c954bf6515f8873124e8558046e10f
Christian Martini <christian.martini@mercadolibre.com>
2021-08-30 22:54:37 -0300

    versión 20210830

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MainNav from './components/MainNav/MainNav';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
+  
+  const onAdd = (count) => {
+    alert(`Ud a agergado ${count} unidades al carrito`);
+  };
+
   return (
     <div className="App">
       <MainNav />
@@ -14,9 +20,10 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
         <h1>Bienvenido a TiendaCompras</h1>
-        <ItemListContainer name="Christian" />
+        <ItemListContainer name="Christian"></ItemListContainer>
+        <ItemCount initial={1} stock={10} onAdd={onAdd}></ItemCount>
       </header>
-      <footer class="Footer">
+      <footer className="Footer">
           <p>Autor: Christian Martini</p>
           <p><a href="mailto:christian.martini@outlook.com">christian.martini@outlook.com</a></p>
       </footer>
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+function ItemCount({stock, initial, onAdd}){
+
+    const [count, setCount] = useState(1);
+    
+    const handlerClickSum = () => {
+        setCount(count + 1);
+    };
+    
+    const handlerClickRest = () => {
+        setCount(count - 1);
+    };
+
+    const handlerOnAdd = () => {
+        onAdd(count);
+        setCount(initial);
+    };
+
+    if (count<1){
+        setCount(count + 1);
+        return (
+            alert("no se puede agregar una cantidad menor a uno al carrito")
+        ) 
+    }
+    else if (count>stock)
+    {
+        setCount(count - 1);
+        return (
+            alert("no hay suficiente stock para agregar al carrito")
+        )  
+    }
+    else{
+        return (
+            <div>
+                <h1>Contador</h1>
+                <button className="btn"  onClick={handlerClickRest}>-</button>
+                <label className="btn">{count}</label>
+                <button className="btn" onClick={handlerClickSum}>+</button>
+                <button className="btn" onClick={handlerOnAdd}>Agregar</button>
+            </div>
+        )
+    }
+
+
+    
+};
+
+export default ItemCount
